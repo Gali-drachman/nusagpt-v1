6f2e30283a791c1224d655a42254d62a8ad59c0b
Gali-drachman <gazalidrachman@gmail.com>
2025-03-03 09:29:35 +0000

    Setup awal React & struktur folder dashboard

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to NusaGPT Dashboard</h1>
+      <p>Mulai membangun UI chatbot AI kita!</p>
+    </div>
+  );
+}
+
+export default App;
